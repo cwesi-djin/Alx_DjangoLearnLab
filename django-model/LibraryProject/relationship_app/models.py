9ccb10a6617f97f0ff9645b9bd3563a7f31a7dfb
cwesi-djin <cwesidjin1@gmail.com>
2025-07-18 03:04:21 +0000

    testing

--- django-model/LibraryProject/relationship_app/models.py
+++ django-model/LibraryProject/relationship_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
-    birth_date = models.DateField()
+
 
     def __str__(self):
         return self.name
