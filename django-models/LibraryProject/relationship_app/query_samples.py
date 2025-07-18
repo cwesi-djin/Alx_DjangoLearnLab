7bbcb03d587fe95253a8b88fb03fcb6aded6195d
cwesi-djin <cwesidjin1@gmail.com>
2025-07-18 03:38:47 +0000

    task - 0

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,9 +9,13 @@ django.setup()
 from relationship_app.models import Author, Book, Library, Librarian
 
 # 1. Query all books by a specific author
-def books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    return author.books.all()
+def get_books_by_author(author_id):
+    try:
+        author = Author.objects.get(id=author_id)
+        books = Book.objects.filter(author=author)
+        return books
+    except Author.DoesNotExist:
+        return []
 
 # 2. List all books in a specific library
 def books_in_library(library_name):
@@ -23,15 +27,3 @@ def librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
     return library.librarian
 
-# Sample usage (after populating database via admin or shell)
-if __name__ == "__main__":
-    print("Books by Author 'Chinua Achebe':")
-    for book in books_by_author('Chinua Achebe'):
-        print("-", book.title)
-
-    print("\nBooks in 'Central Library':")
-    for book in books_in_library('Central Library'):
-        print("-", book.title)
-
-    print("\nLibrarian of 'Central Library':")
-    print(librarian_for_library('Central Library').name)
