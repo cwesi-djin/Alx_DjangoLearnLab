cdd533150cc0cb15349df0ecf9a95df85ed3ec2b
cwesi-djin <cwesidjin1@gmail.com>
2025-07-20 04:56:39 +0000

    last commit hopefully again

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView,add_book, edit_book, delete_book
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.list_books, name='book_list'),
     path('books/', list_books, name='list_books'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
+    # Authentication URLs
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register_view, name='register'),  # updated to correct function name
@@ -16,7 +17,7 @@ urlpatterns = [
     path('librarian-panel/', views.librarian_view, name='librarian_view'),
     path('member-panel/', views.member_view, name='member_view'),
 
-    path('books/add/', add_book, name='add_book'),
-    path('books/edit/<int:book_id>/', edit_book, name='edit_book'),
-    path('books/delete/<int:book_id>/', delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
