597eeda0c52f662a5ebbfc2d313627b51e2f1428
cwesi-djin <cwesidjin1@gmail.com>
2025-07-27 10:55:02 +0000

    # task 2. Implementing Security Best Practices in Django test

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,3 +2,5 @@ from django import forms
 
 class BookSearchForm(forms.Form):
     q = forms.CharField(max_length=100)
+
+
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required
 from .models import Book
 
 # Create your views here.
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from .models import Book
-
-from django.contrib.auth.decorators import permission_required
 
 def book_search(request):
     query = request.GET.get('q', '')
@@ -18,3 +13,12 @@ def book_search(request):
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
+
+def book_search(request):
+    form = BookSearchForm(request.GET)
+    if form.is_valid():
+        query = form.cleaned_data['q']
+        books = Book.objects.filter(title__icontains=query)
+    else:
+        books = Book.objects.none()
+    return render(request, 'bookshelf/book_list.html', {'form': form, 'books': books})
