b35bf769e5c4f1c3065cadb3193ce8e2b017f5e5
cwesi-djin <cwesidjin1@gmail.com>
2025-07-27 15:44:56 +0000

    # task 2. Implementing Security Best Practices in Django test..

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,6 +1,11 @@
 from django import forms
+from .models import Book
 
 class BookSearchForm(forms.Form):
     q = forms.CharField(max_length=100)
 
 
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date']
