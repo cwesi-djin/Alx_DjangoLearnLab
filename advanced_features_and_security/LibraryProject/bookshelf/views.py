1787452f31c7097f3c7fde66f8f9cca01663a8e5
cwesi-djin <cwesidjin1@gmail.com>
2025-07-27 15:48:17 +0000

    # task 2. Implementing Security Best Practices in Django test..2

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 from .forms import BookSearchForm
+from .forms import ExampleForm
+
 
 # Create your views here.
 
@@ -23,3 +25,13 @@ def book_search(request):
     else:
         books = Book.objects.none()
     return render(request, 'bookshelf/book_list.html', {'form': form, 'books': books})
+
+def example_form_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success_page')  # define this route or use another
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
