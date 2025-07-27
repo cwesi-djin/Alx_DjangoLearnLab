e677b425e543559e6801da209238c485c51850d9
cwesi-djin <cwesidjin1@gmail.com>
2025-07-27 10:47:22 +0000

    # task 2. Implementing Security Best Practices in Django test

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -9,6 +9,11 @@ from .models import Book
 
 from django.contrib.auth.decorators import permission_required
 
+def book_search(request):
+    query = request.GET.get('q', '')
+    books = Book.objects.filter(title__icontains=query)
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
