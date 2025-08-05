dd4d2067665081588f549440067a8a8463c5e7bb
cwesi-djin <cwesidjin1@gmail.com>
2025-08-05 09:11:15 +0000

    # task 1. Building Custom Views and Generic Views in Django REST Framework testing...

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,29 +2,31 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here.
 
 class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = IsAuthenticatedOrReadOnly
 
 class DetailView(generics.RetrieveAPIView):
     queryset  = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = IsAuthenticated
 
 class CreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
+    permission_classes = IsAuthenticated
 
 class UpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = IsAuthenticated
 
 class DeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = IsAuthenticatedOrReadOnly
